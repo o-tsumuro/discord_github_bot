214dc6a02a0635c860a73f2e69b8a4dbb8339b13
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-17 10:47:57 +0900

    bot/index.js - Botの起動処理

---
+++ bot/index.js
@@ -0,0 +1,14 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds], // サーバーの基本情報を取得するためのIntent
+});
+
+// 起動時の処理
+client.once('ready', () => {
+  console.log(`✅ Bot がログインしました: ${client.user.tag}`);
+});
+
+// トークンでログイン
+client.login(process.env.DISCORD_TOKEN);
