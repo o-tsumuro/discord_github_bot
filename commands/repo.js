5cf6ceb239c864f47d3a7317bf3e90c851010ecd
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-17 15:12:40 +0900

    commands/repo.js - urlの保存機能

--- commands/repo.js
+++ commands/repo.js
@@ -1,4 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const CONFIG_FILE = path.join(__dirname, '../data/repo-config.json');
+
+function saveRepoURL(guildId, url) {
+  let data = {};
+  if (fs.existsSync(CONFIG_FILE)) {
+    data = JSON.parse(fs.readFileSync(CONFIG_FILE, 'utf8'));
+  }
+
+  data[guildId] = { url };
+
+  fs.writeFileSync(CONFIG_FILE, JSON.stringify(data, null, 2));
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,8 +23,13 @@ module.exports = {
     option.setName('url')
       .setDescription('GitHubのリポジトリのURLを入力してください')
       .setRequired(true)),
+
     async execute(interaction) {
       const url = interaction.options.getString('url');
+      const guildId = interaction.guildId;
+
+      saveRepoURL(guildId, url);
+      
       await interaction.reply(`✅ GitHubリポジトリを登録しました: ${url}`);
     }
 };
