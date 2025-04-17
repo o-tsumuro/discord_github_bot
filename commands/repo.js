0bdc81eb45f3e61c40f82c3b087c66f23608cd13
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-17 11:15:37 +0900

    commands/repo.js - /repoコマンド定義

---
+++ commands/repo.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName('repo')
+  .setDescription('通知う対象のGitHubリポジトリを設定します')
+  .addStringOption(option =>
+    option.setName('url')
+      .setDescription('GitHubのリポジトリのURLを入力してください')
+      .setRequired(true)),
+    async execute(interaction) {
+      const url = interaction.option.getString('url');
+      await interaction.reply(`✅ GitHubリポジトリを登録しました: ${url}`);
+    }
+};
