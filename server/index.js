3fd39d07798e021a456b65f24c9e2dc15d3ca8d2
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-18 11:52:55 +0900

    デバッグコンソール

--- server/index.js
+++ server/index.js
@@ -32,6 +32,9 @@ app.post('/webhook', (req, res) => {
     if (entry) {
       const [guildId, { channelId }] = entry;
       const channel = client.channels.cache.get(channelId);
+      console.log('🔍 探してるチャンネルID:', channelId);
+      console.log('📦 client.channels.cache.has(channelId):', client.channels.cache.has(channelId));
+      console.log('🧾 全チャンネル一覧:', [...client.channels.cache.keys()]);
       if (channel && channel.isTextBased()) {
         channel.send(message);
       } else {
