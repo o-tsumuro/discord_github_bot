cb63dc1745b7620fe17ca1dd4a0f7ec1dbb3aa39
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-18 11:07:58 +0900

    repo.js - 通知先チャンネルの保存

--- commands/repo.js
+++ commands/repo.js
@@ -4,32 +4,34 @@ const path = require('path');
 
 const CONFIG_FILE = path.join(__dirname, '../data/repo-config.json');
 
-function saveRepoURL(guildId, url) {
+function saveRepoURL(guildId, channelId, url) {
   let data = {};
   if (fs.existsSync(CONFIG_FILE)) {
     data = JSON.parse(fs.readFileSync(CONFIG_FILE, 'utf8'));
   }
 
-  data[guildId] = { url };
+  data[guildId] = { url, channelId };
 
   fs.writeFileSync(CONFIG_FILE, JSON.stringify(data, null, 2));
 }
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName('repo')
-  .setDescription('通知う対象のGitHubリポジトリを設定します')
-  .addStringOption(option =>
-    option.setName('url')
-      .setDescription('GitHubのリポジトリのURLを入力してください')
-      .setRequired(true)),
+    .setName('repo')
+    .setDescription('GitHubリポジトリを設定します')
+    .addStringOption(option =>
+      option.setName('url')
+        .setDescription('GitHubのリポジトリのURLを入力してください')
+        .setRequired(true)
+    ),
 
-    async execute(interaction) {
-      const url = interaction.options.getString('url');
-      const guildId = interaction.guildId;
+  async execute(interaction) {
+    const url = interaction.options.getString('url');
+    const guildId = interaction.guildId;
+    const channelId = interaction.channelId;
 
-      saveRepoURL(guildId, url);
+    saveRepoURL(guildId, channelId, url);
       
-      await interaction.reply(`✓ GitHubリポジトリを登録しました: ${url}`);
-    }
+    await interaction.reply(`✓ GitHubリポジトリを登録しました: ${url}`);
+  }
 };
